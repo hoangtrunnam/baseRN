bc605c4ae7e77017592c23d750e51e71b1024da9
nam hoang <hoangtrungnam0000@gmail.com>
2023-10-10 08:22:02 +0700

    fix: absolute path again

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,21 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      require.resolve('babel-plugin-module-resolver'),
+      'module-resolver',
       {
         root: ['./src'],
+        alias: {
+          'src/api': './src/api',
+          'src/assets': './src/assets',
+          'src/commons': './src/commons',
+          'src/components': './src/components',
+          'src/configs': './src/configs',
+          'src/constants': './src/constants',
+          'src/features': './src/features',
+          'src/locales': './src/locales',
+          'src/navigation': './src/navigation',
+          'src/redux': './src/redux',
+        },
       },
     ],
   ],
