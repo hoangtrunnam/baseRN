86f8363c8235c6fcc5653bb86bcd8e31c46f3970
nam hoang <hoangtrungnam0000@gmail.com>
2023-10-19 15:36:09 +0700

    chore: remove baseUrlRocketChat

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,5 @@
 export const baseUrl = 'https://api.publicapis.org' // dev
 
-export const baseUrlRocketChat = 'https://esupports-dev.epicpartner.vn' // dev
-
 export const timeout = 1200000 // 120s
 
 export const apiEntries = '/entries'
